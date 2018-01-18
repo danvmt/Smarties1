329e6009ba123ded4f20587e9c74d47cf9e357af
dan <danndeli_11@hotmail.com>
2018-01-18 16:21:23 +0100

    init

---
+++ Smarties/SmartiesPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Smarties
+{
+    public partial class SmartiesPage : ContentPage
+    {
+        public SmartiesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
