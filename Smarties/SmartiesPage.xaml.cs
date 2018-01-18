6fdc89b325973310acf15e88622ec0e86731a017
dan <danndeli_11@hotmail.com>
2018-01-18 18:24:30 +0100

    blub

--- Smarties/SmartiesPage.xaml.cs
+++ Smarties/SmartiesPage.xaml.cs
@@ -1,12 +1,35 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
 
 namespace Smarties
 {
     public partial class SmartiesPage : ContentPage
     {
+
+        ObservableCollection<ListItem> listItems = new ObservableCollection<ListItem>();
+
+
         public SmartiesPage()
         {
             InitializeComponent();
+
+            listView.ItemsSource = listItems;
+
+            listItems.Add(new ListItem { Text = "Room 1", Detail = "Living Room", Image  = "livingroom.png" });
+            listItems.Add(new ListItem { Text = "Room 2", Detail = "Bed Room" , Image = "bedroom.png"});
+            listItems.Add(new ListItem { Text = "Room 3", Detail = "Office", Image = "office.png" });
+
+        }
+
+        public void AddItem(string itemText, string detailText, string ImageUrl)
+        {
+            listItems.Add(new ListItem { Text = itemText, Detail = detailText, Image = ImageUrl });
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NewRoomPage());
         }
     }
 }
